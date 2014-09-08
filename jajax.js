c5549aa0531165870085e7e7130f2655ef85069c
Alejo Lucangeli <alejolucangeli@gmail.com>
2014-09-07 21:06:30 -0300

    Methods outline

--- jajax.js
+++ jajax.js
@@ -9,7 +9,7 @@ var jajax = (function(){
         return destination;
     }
 
-    function createRequestObject(){
+    function requestObjectFactory(){
 
     }
 
@@ -27,7 +27,7 @@ var jajax = (function(){
 
     var Request = (function(){
         /**
-         * A Request object
+         * A Request object.
          * @class Request
          * @constructor
          * @param {String} method **GET** or **POST**. Defaults to **GET**
@@ -36,17 +36,60 @@ var jajax = (function(){
             this.method = (typeof method !== 'undefined') ? method : 'GET';
             this.parameters = {};
 
-            this.beforeRequest = null;
-            this.afterRequest = null;
-            this.success = null;
-            this.error = null;
-            this.complete = null;
+            this.onSuccess = null;
+            this.onError = null;
+            this.onComplete = null;
+
+            this.requestObject = requestObjectFactory();
+        }
+        /**
+         * @method setRequestMethod
+         * @param {String} method **GET** or **POST**
+         */
+        Request.prototype.setRequestMethod(method){
+            this.method = method;
         }
 
+        /**
+         * Sets the request parameters. If the request method is **GET** and the request URL already
+         * contains parameters, the parameters passed to this function will be appended at the end.
+         * @method setParameters
+         * @param {Object} parameters
+         */
         Request.prototype.setParameters(parameters){
             this.parameters = parameters;
         }
-        Request.protoype.setBeforeRequestCallback
+        /**
+         * Sets the callback to be executed if the request succeeds
+         * @method onSuccess
+         * @param {Function} callbackFunction
+         */
+        Request.prototype.onSuccess = function(callbackFunction){
+            this.onSuccess = callbackFunction;
+        }
+        /**
+         * Sets the callback to be executed if the request fails
+         * @method onError
+         * @param {Function} callbackFunction
+         */
+        Request.prototype.onError = function(callbackFunction){
+            this.onError = callbackFunction;
+        }
+        /**
+         * Sets the callback to be executed right after **success** or **error**
+         * @method onComplete
+         * @param {Function} callbackFunction
+         */
+        Request.prototype.onComplete = function(callbackFunction){
+            this.onComplete = callbackFunction;
+        }
+        /**
+         * Executes the Request
+         * @method execute
+         */
+         Request.prototype.execute = function(){
+            // Calls the ajax() function
+         }
         return Request;
     }());
 
